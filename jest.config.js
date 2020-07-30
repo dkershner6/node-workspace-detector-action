2740609b4ffd7faa60827442fd41e56b4f519e3a
Derek Kershner <dkershner@cbtnuggets.com>
2020-07-30 14:38:58 -0700

    Revert "Functionality present, tested (somewhat, tough to test), README, action.yml"
    This reverts commit 883091002f31550852e5e252f25bb2fd65fd2183.

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,11 @@
 module.exports = {
-    clearMocks: true,
-    moduleFileExtensions: ['js', 'ts'],
-    testEnvironment: 'node',
-    testMatch: ['**/*.test.ts'],
-    testRunner: 'jest-circus/runner',
-    transform: {
-        '^.+\\.ts$': 'ts-jest',
-    },
-    verbose: true,
-    collectCoverageFrom: ['src/**/*.ts'],
-    coveragePathIgnorePatterns: [
-        '/node_modules/',
-        '/dist/',
-        '/test/',
-        '/lib/',
-        '/src/main.ts',
-    ],
-};
+  clearMocks: true,
+  moduleFileExtensions: ['js', 'ts'],
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  testRunner: 'jest-circus/runner',
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  verbose: true
+}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
-import { setFailed, setOutput } from '@actions/core';
-import findPackageJson from './findPackageJson';
+import * as core from '@actions/core'
+import {wait} from './wait'
 
 async function run(): Promise<void> {
-    try {
-        const result = findPackageJson();
+  try {
+    const ms: string = core.getInput('milliseconds')
+    core.debug(`Waiting ${ms} milliseconds ...`)
 
-        setOutput('workspace', result);
-    } catch (error) {
-        setFailed(error.message);
-    }
+    core.debug(new Date().toTimeString())
+    await wait(parseInt(ms, 10))
+    core.debug(new Date().toTimeString())
+
+    core.setOutput('time', new Date().toTimeString())
+  } catch (error) {
+    core.setFailed(error.message)
+  }
 }
 
-run();
+run()
